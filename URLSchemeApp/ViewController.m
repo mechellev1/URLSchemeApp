c98036fbfda7e0c4b185546f4fe7c9fc5b4ed706
Mechelle Sieglitz <mechelle.sieglitz@v1sports.com>
2019-02-28 13:56:42 -0500

    set up to receive params

---
+++ URLSchemeApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  URLSchemeApp
+//
+//  Created by Mechelle Sieglitz on 2/28/19.
+//  Copyright © 2019 Mechelle Sieglitz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
